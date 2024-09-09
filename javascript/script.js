50c40fcd1873e6daa89644dfa6dea8f87d7a784e
Lun-github-star <lunlun062@gmail.com>
2024-09-09 15:49:48 +0700

    first commit

---
+++ javascript/script.js
@@ -0,0 +1,14 @@
+'use strict'
+console.log("hello world")
+
+// console.clear()
+// console.log("Console log")
+// console.error("Console Error")
+// console.info("Console info")
+// console.group("This is a group")
+// console.log("Hello")
+// console.log("Hi")
+// console.groupEnd()
+// console.log("Hi")
+console.log(%c Hello World)
+let changeText = document.querySelector("h1")
